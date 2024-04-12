89adcd95494fa5fd10a22b145c90d37c16285c2a
hoipranav <hoipranavv@gmail.com>
2024-04-12 22:41:38 +0530

    Add dynamic url's

--- mysite/gym/urls.py
+++ mysite/gym/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("dashboard", views.dashboard, name="dashboard"),
+    path("register/trainer", views.reg_trainer, name="reg_trainer"),
+    path("register/member/", views.reg_member, name="reg_member"),
+    path("member/information", views.member_info, name="member_info"),
+    path("attendance/", views.attendance, name="attendance"),
+    path("dashboard/", views.dashboard, name="dashboard"),
     path("", views.home, name="home")
 ]
--- mysite/gym/views.py
+++ mysite/gym/views.py
@@ -6,5 +6,21 @@ def home(request):
     return HttpResponse(template.render())
 
 def dashboard(request):
-    template = loader.get_template("gym/index2.html")
+    template = loader.get_template("gym/dashboard.html")
+    return HttpResponse(template.render())
+
+def reg_trainer(request):
+    template = loader.get_template("gym/reg_trainer.html")
+    return HttpResponse(template.render())
+
+def reg_member(request):
+    template = loader.get_template("gym/reg_member.html")
+    return HttpResponse(template.render())
+
+def member_info(request):
+    template = loader.get_template("gym/member_info.html")
+    return HttpResponse(template.render())
+
+def attendance(request):
+    template = loader.get_template("gym/attendance.html")
     return HttpResponse(template.render())
